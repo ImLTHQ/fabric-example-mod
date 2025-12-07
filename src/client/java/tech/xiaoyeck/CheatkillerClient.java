1c4268b8b2d88a423bc8acc1c30640693f0c0934
1 <1@1.com>
2025-12-07 22:27:46 +0800

    1

---
+++ src/client/java/tech/xiaoyeck/CheatkillerClient.java
@@ -0,0 +1,10 @@
+package tech.xiaoyeck;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CheatkillerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
